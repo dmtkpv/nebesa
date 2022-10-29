c7bb9701ed2e00d32e1da0fc14d8942568ef56b4
Dmitriy Karpov <dmtkpv@gmail.com>
2022-10-29 09:54:09 +0300

    0.0.2-1

--- src/index.vue
+++ src/index.vue
@@ -8,7 +8,13 @@
         position: fixed;
         inset: 0;
         perspective: 700px;
+        perspective-origin: 50% 0;
+        animation: app 5s ease-in-out infinite;
     }
+    @keyframes app {
+        /*50% { perspective-origin: 50% 50% }*/
+    }
+
     .app div {
         position: absolute;
         inset: 0;
@@ -16,33 +22,32 @@
     }
 
     .app .view {
-        /*transform: rotateY(0deg) translateX(50%);*/
+        transform: translateY(80%);
         animation: view 5s ease-in-out infinite;
     }
     @keyframes view {
-        /*0% { transform: rotateY(0deg) translateX(0%) }*/
-        50% { transform: rotateY(90deg) translate3d(50%, 0, 350px) }
+        50% { transform: translateY(0%) }
     }
 
 
     .app .title {
         display: flex;
-        align-items: center;
+        /*align-items: center;*/
         justify-content: center;
         font-size: 10vw;
         font-weight: bold;
-        transform: translateZ(-350px);
+        transform: translateZ(-250px);
     }
 
     .app .clouds {
         background: rgba(0,0,255,.2);
     }
-    .app .clouds:nth-child(2) { transform: translateZ(-100px) }
-    .app .clouds:nth-child(3) { transform: translateZ(-200px) }
-    .app .clouds:nth-child(4) { transform: translateZ(-300px) }
-    .app .clouds:nth-child(5) { transform: translateZ(-400px) }
-    .app .clouds:nth-child(6) { transform: translateZ(-500px) }
-    .app .clouds:nth-child(7) { transform: translateZ(-600px) }
+    .app .clouds:nth-child(2) { transform: translateZ(-0px) }
+    .app .clouds:nth-child(3) { transform: translateZ(-100px) }
+    .app .clouds:nth-child(4) { transform: translateZ(-200px) }
+    .app .clouds:nth-child(5) { transform: translateZ(-300px) }
+    .app .clouds:nth-child(6) { transform: translateZ(-400px) }
+    .app .clouds:nth-child(7) { transform: translateZ(-500px) }
 
 </style>
 
